e03a468d9ea943accbb01f902dc287f72407e94a
Karri Kuivanen <kamiheku@gmail.com>
2016-03-08 14:29:09 +0200

    it works!

--- sonaatti.py
+++ sonaatti.py
@@ -4,52 +4,56 @@ from bs4 import BeautifulSoup
 from sys import argv
 import requests
 import re
+import click
 
-ravintolat = {'lozzi':       'http://www.sonaatti.fi/lozzi/',
-              'syke':        'http://www.sonaatti.fi/syke/',
-              'tilia':       'http://www.sonaatti.fi/ravintola_t/',
-              'kvarkki':     'http://www.sonaatti.fi/kvarkki/',
-              'ylisto':      'http://www.sonaatti.fi/ylisto/',
-              'piato':       'http://www.sonaatti.fi/piato/',
-              'wilhelmiina': 'http://www.sonaatti.fi/wilhelmiina/',
-              'uno':         'http://www.sonaatti.fi/uno/'}
+restaurants = {'lozzi':       'http://www.sonaatti.fi/lozzi/',
+               'syke':        'http://www.sonaatti.fi/syke/',
+               'tilia':       'http://www.sonaatti.fi/ravintola_t/',
+               'kvarkki':     'http://www.sonaatti.fi/kvarkki/',
+               'ylisto':      'http://www.sonaatti.fi/ylisto/',
+               'piato':       'http://www.sonaatti.fi/piato/',
+               'wilhelmiina': 'http://www.sonaatti.fi/wilhelmiina/',
+               'uno':         'http://www.sonaatti.fi/uno/'}
 
-def stripEmpty(ruuat):
-    while None in ruuat:
-        ruuat.remove(None)
-    return ruuat
+def stripEmpty(foods):
+    while None in foods:
+        foods.remove(None)
+    return foods
 
-def stripSpecs(ruoka):
+def stripSpecs(food):
     # Let's strip the *'s ("healthy choice")
-    ruoka = ruoka.replace("*", "")
+    food = food.replace("*", "")
     # Random annoying inconcistencies
-    ruoka = ruoka.replace("Veg", "")
-    # ruoka = re.sub("  ", " ", ruoka)
-    ruoka = ruoka.replace("  ", " ")
+    food = food.replace("Veg", "")
+    # food = re.sub("  ", " ", food)
+    food = food.replace("  ", " ")
     # And the dietary stuff too
     exp = "#[A-Z#, ]*( |$)"
-    ruoka = re.sub(exp, " ", ruoka)
-    ruoka = re.sub("  +", " ", ruoka)
-    return ruoka
+    food = re.sub(exp, " ", food)
+    food = re.sub("  +", " ", food)
+    return food
 
-def getHtmlData(ravintola):
-    r = requests.get("http://www.sonaatti.fi/%s/" %ravintola)
+def getHtmlData(restaurant):
+    url = restaurants[restaurant]
+    r = requests.get(url)
     htmldata = r.text
     return htmldata
 
-def getFoods(ravintola):
-    htmldata = getHtmlData(ravintola)
-    keitto = BeautifulSoup(htmldata, "html.parser")
-    ruuat = keitto.find(class_='ruuat')
-    ruuat = ruuat.find_all('p')
-    ruuat = list(map(lambda x: x.string, ruuat))
-    ruuat = stripEmpty(ruuat)
-    ruuat = list(map(lambda x: stripSpecs(x), ruuat))
-    for ruoka in ruuat:
-        print(ruoka)
+@click.command()
+@click.argument('restaurant')
+def getFoods(restaurant):
+    htmldata = getHtmlData(restaurant)
+    soup = BeautifulSoup(htmldata, "html.parser")
+    foods = soup.find(class_='ruuat')
+    foods = foods.find_all('p')
+    foods = list(map(lambda x: x.string, foods))
+    foods = stripEmpty(foods)
+    foods = list(map(stripSpecs, foods))
+    for food in foods:
+        print(food)
 
-def main():
-    getFoods(argv[-1])
+# def main():
+#     getFoods(restaurant)
 
 if __name__ == "__main__":
-    main()
+    getFoods()
