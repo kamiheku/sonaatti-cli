1c1e87f067c5538b0f806f59ab20bac8d6749fe2
Karri Kuivanen <kamiheku@gmail.com>
2017-04-20 23:32:25 +0300

    cleanup

--- sonaatti.py
+++ sonaatti.py
@@ -6,30 +6,29 @@ Usage: sonaatti.py RESTAURANT
 Fetches the menu for a given Sonaatti student restaurant from sonaatti.fi.
 """
 
-import urllib.request
-import time
-import json
-import click
-from sys import exit
 from datetime import date
 from collections import OrderedDict
+import urllib.request
+import json
+import click
 
-RESTAURANT_IDS = { 
-    "libri" : "137814",
-    "lozzi" : "137828",
-    "syke" : "137833",
-    "tilia" : "137838",
-    "kvarkki" : "137876",
-    "ylisto" : "137871",
-    "piato" : "110874",
-    "wilhelmiina" : "137866",
-    "uno" : "137861",
-    "normaalikoulu" : "138323",
-    "novelli" : "137881"
+RESTAURANT_IDS = {
+    "libri": "137814",
+    "lozzi": "137828",
+    "syke": "137833",
+    "tilia": "137838",
+    "kvarkki": "137876",
+    "ylisto": "137871",
+    "piato": "110874",
+    "wilhelmiina": "137866",
+    "uno": "137861",
+    "normaalikoulu": "138323",
+    "novelli": "137881"
 }
 
 API_URL = "http://www.sonaatti.fi/api/restaurant/menu/week?language=fi&restaurantPageId={}&weekDate={}"
 TODAY = date.today().strftime("%-d.%-m.%Y")
+
 
 def getdata(restaurant):
     """Gets JSON for the specified restaurant and converts to dict
@@ -51,6 +50,7 @@ def getdata(restaurant):
         print("Couldn't fetch data for restaurant :(")
         exit(1)
 
+
 def parsedata(data):
     """Parses a dict containing the data for a restaurant, out comes an
     OrderedDict with lunches/meals
@@ -66,7 +66,7 @@ def parsedata(data):
     for day in data['LunchMenus']:
         if day['Date'] == TODAY:
             for lunch in day['SetMenus']:
-                if lunch['Name'] != None:
+                if lunch['Name'] is not None:
                     lunch_name = lunch['Name']
                     if lunch_name not in lunches.keys():
                         lunches[lunch_name] = []
@@ -75,6 +75,7 @@ def parsedata(data):
             break
 
     return lunches
+
 
 def printdata(lunches):
     """Prettyprints the given lunch data
@@ -90,14 +91,24 @@ def printdata(lunches):
         for meal in (lunches[lunch]):
             print(" " * 4 + meal)
 
+
 @click.command()
 @click.argument('restaurant')
 def getandprint(restaurant):
+    """Gets and prints the menu for the given restaurant
+
+    Args:
+        restaurant (str): Name of the restaurant
+
+    Returns:
+        None
+    """
     restaurant = restaurant.lower()
     if restaurant not in RESTAURANT_IDS.keys():
-        print('Restaurant "{}" not found. Valid restaurants:'.format(restaurant))
-        for r in RESTAURANT_IDS.keys():
-            print(r.title())
+        print('Restaurant "{}" not found. Valid restaurants:'.format(
+            restaurant))
+        for restaurant_id in RESTAURANT_IDS:
+            print(restaurant_id.title())
         exit(1)
 
     data = getdata(restaurant)
@@ -105,6 +116,7 @@ def getandprint(restaurant):
 
     print(restaurant.title() + '\n')
     printdata(lunches)
+
 
 if __name__ == "__main__":
     getandprint()
